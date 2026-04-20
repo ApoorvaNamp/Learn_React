9238652d7b3b14896aa3c3d4ec616a043a88e1b8
ApoorvaN <apoorva30nampalliwar@gmail.com>
2026-04-20 16:58:29 +0530

    react component

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Learn React 
+
+# parcel
+-dev build
+-provides local server
+-HMR (Hot Module Replacement)
+-use file watching algorithm
+-caching for faster build
+-image optimisation
+-bundling of file
+-compressing the file
+-consistent hashing
+-code splitting
+-differential bundling: supports older browser
+-diagnostic
+-error handling
+
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 /**
  * 
  * <div>
@@ -14,18 +17,30 @@
  * /div>
  */
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" },[
-        React.createElement("h1", {}, "Hello World from React!!!!!!"),
-        React.createElement("h2", {}, "Heading 2"),
-        ]),
+// const parent = React.createElement("div", { id: "parent" }, [
+//     React.createElement("div", { id: "child1", key: "child1" }, [
+//         React.createElement("h1", { key: "h1-1", id: "heading1" }, "Hello World from React Heading 1!!!!!!"),
+//         React.createElement("h2", { key: "h2-1", id: "heading2" }, "Heading 2"),
+//     ]),
 
-    React.createElement("div", { id: "child2" },[
-        React.createElement("h1", {}, "Hello World from React!!!!!!"),
-        React.createElement("h2", {}, "Heading 2"),
-        ]),
-]);
+//     React.createElement("div", { id: "child2", key: "child2" }, [
+//         React.createElement("h1", { key: "h1-2", id: "heading3" }, "Hello World from React!!!!!!"),
+//         React.createElement("h2", { key: "h2-2", id: "heading4" }, "Heading 4"),
+//     ]),
+// ]);
+
+
+// const heading=<h1 id="heading1">Hello World from React Heading 1!!!!!!</h1>
+
+const title = "Heading 1 from React";
+
+const Heading = ()=>(
+    <div id="div">
+        {title}
+    <h1 id="heading1">Hello World from React Heading 1!!!!!!</h1>
+    </div>
+);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(<Heading />);
